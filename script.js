371928c489ed60800c1b6ef7cfe76beb13d8bcf9
VeyselYazici <veyselkaraniyazici@gmail.com>
2023-09-14 13:44:25 +0300

    ilk yükleme

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("login-form");
+  
+    loginForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+  
+      // Burada giriş işlemlerini kontrol etmek için gerekli kodları ekleyebilirsiniz
+    });
+  });
