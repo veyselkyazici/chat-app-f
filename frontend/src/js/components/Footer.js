e18c966be4fe4c3410c0de4d3a565569494a056d
Veysel YAZICI <veyselkaraniyazici@gmail.com>
2025-11-20 20:46:28 +0300

    11.20.2025 4

--- frontend/src/js/components/Footer.js
+++ frontend/src/js/components/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => `
   <footer class="footer">
     <div class="social-icons">
-      <a href="#" class="icon"><i class="fa-brands fa-instagram"></i></a>
+      <a href="https://www.instagram.com/veyselyzici" target="_blank" rel="noopener noreferrer" class="icon"><i class="fa-brands fa-instagram"></i></a>
       <a href="https://github.com/veyselkyazici" class="icon"    target="_blank" 
    rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
       <a href="https://www.linkedin.com/in/veyselkaraniyazici" 
